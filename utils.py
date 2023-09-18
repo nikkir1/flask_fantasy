ded566149236be0b1674f6cf521280e5ef11e457
nikkir1 <127956692+nikkir1@users.noreply.github.com>
2023-09-18 08:30:04 +0300

    ьь

--- utils.py
+++
@@ -1,9 +0,0 @@
-def initDB(conn):
-    c = conn.cursor()
-
-    c.execute('''CREATE TABLE IF NOT EXISTS orders
-                 (id integer, product integer, datetime text, user real, code integer, status integer, delivery_point integer)''')
-    c.execute('''CREATE TABLE IF NOT EXISTS users
-                     (id integer, name string, count_orders int)''')
-
-    conn.commit()
